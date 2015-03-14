9010cd698de6cc0ad0e8cd3971d0f54709c49c21
Hai Li <leehigh0316@Hotmail.com>
2015-03-14 11:38:15 -0500

    add adapter, select account view

---
+++ app/src/main/java/umn/cloud/MobileArrayAdapter.java
@@ -0,0 +1,47 @@
+package umn.cloud;
+
+/**
+ * Created by AngusY on 3/14/15.
+ */
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MobileArrayAdapter extends ArrayAdapter<String> {
+    private final Context context;
+    private final String[] values;
+
+    public MobileArrayAdapter(Context context, String[] values) {
+        super(context, R.layout.activity_add_service_account, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.activity_add_service_account, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.label);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.logo);
+        textView.setText(values[position]);
+
+        // Change icon based on name
+        String s = values[position];
+
+        System.out.println(s);
+
+        if (s.equals("DropBox")) {
+            imageView.setImageResource(R.drawable.dropboxIcon);
+        } else if (s.equals("GoogleDoc")) {
+            imageView.setImageResource(R.drawable.googleDocIcon);
+        }
+
+        return rowView;
+    }
+}
---
+++ app/src/main/java/umn/cloud/selectTargetAccount.java
@@ -0,0 +1,31 @@
+package umn.cloud;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.view.View;
+
+public class selectTargetAccount extends ListActivity {
+
+    static final String[] MOBILE_OS =
+            new String[] { "Android", "iOS", "WindowsMobile", "Blackberry"}; //modified needed
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setListAdapter(new MobileArrayAdapter(this, MOBILE_OS));
+
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+
+        //get selected items
+        String selectedValue = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
+
+    }
+
+}
