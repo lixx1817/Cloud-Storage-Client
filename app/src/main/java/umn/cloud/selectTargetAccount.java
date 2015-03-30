829b46acccac5a4e7ec51cf83d2387b8eb4c38df
Hai Li <leehigh0316@Hotmail.com>
2015-03-30 17:48:37 -0500

    add checkbox and save progress

--- app/src/main/java/umn/cloud/selectTargetAccount.java
+++ app/src/main/java/umn/cloud/selectTargetAccount.java
@@ -3,16 +3,18 @@ package umn.cloud;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class selectTargetAccount extends ListActivity {
 
     public accountListAdapter adpt;
     public static final String list_url = "http://ec2-54-213-7-206.us-west-2.compute.amazonaws.com:8080/list_srvacc";
 //    public final ProgressDialog dialog = new ProgressDialog(selectTargetAccount.this);
+    List<serviceAccount> userChoices= new ArrayList<serviceAccount>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,8 @@ public class selectTargetAccount extends ListActivity {
         adpt  = new accountListAdapter(new ArrayList<serviceAccount>(), this);
         ListView lView = (ListView) findViewById(android.R.id.list);
 
+        lView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        lView.setItemsCanFocus(false);
         lView.setAdapter(adpt);
 
         // Exec async load task
@@ -32,8 +36,17 @@ public class selectTargetAccount extends ListActivity {
     protected void onListItemClick(ListView l, View v, int position, long id) {
 
         //get selected items
-        String selectedValue = (String) getListAdapter().getItem(position);
-        Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
+        if (v != null) {
+            CheckedTextView checkBox = (CheckedTextView)v.findViewById(R.id.service_name);
+            checkBox.toString();
+            if(checkBox.isChecked()) {
+                userChoices.add(adpt.getItem(position));
+            }
+        }
+        //serviceAccount selectedValue =  adpt.getItem(position);
+        //userChoices.add(selectedValue);
+
+        //Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
 
     }
 
